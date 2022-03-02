f4d7921357e033022803d43c14c5df1f19157960
YanC1120 <94077850+YanC1120@users.noreply.github.com>
2022-03-02 12:51:59 -0500

    Add files via upload

---
+++ Lab3/ModuladorAM.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: ModuladorAM_J1A
+# Author: J1A_G5
+# GNU Radio version: 3.10.1.1
+
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+
+
+
+
+
+
+
+class ModuladorAM(gr.hier_block2):
+    def __init__(self, Ac=1, ka=1):
+        gr.hier_block2.__init__(
+            self, "ModuladorAM_J1A",
+                gr.io_signature(1, 1, gr.sizeof_float*1),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.Ac = Ac
+        self.ka = ka
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_null_source_0 = blocks.null_source(gr.sizeof_float*1)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_cc(Ac)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(ka)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff(1)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_const_vxx_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self, 0))
+        self.connect((self.blocks_null_source_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self, 0), (self.blocks_multiply_const_vxx_0, 0))
+
+
+    def get_Ac(self):
+        return self.Ac
+
+    def set_Ac(self, Ac):
+        self.Ac = Ac
+        self.blocks_multiply_const_vxx_1.set_k(self.Ac)
+
+    def get_ka(self):
+        return self.ka
+
+    def set_ka(self, ka):
+        self.ka = ka
+        self.blocks_multiply_const_vxx_0.set_k(self.ka)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
